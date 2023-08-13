c7d0018c59e1bb48798397ce5bdde3da9131940a
Ola Næss Kaldestad <mail@okal.no>
2023-08-13 21:55:03 +0200

    improve open helper

--- lib/open.ts
+++ lib/open.ts
@@ -1,4 +1,8 @@
 export const open = async (socket: WebSocket) => {
+    if (socket.readyState === WebSocket.OPEN) {
+        return;
+    }
+
     await new Promise((resolve, reject) => {
         socket.onopen = resolve;
         socket.onerror = reject;
